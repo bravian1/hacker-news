0b66c8c2dd2fe080c7fe514ef37e5ffae22b7f83
bravian1 <nyatorobravian@gmail.com>
2024-09-14 23:53:45 +0300

    fix: fix how empty comments are handled

--- app.js
+++ app.js
@@ -43,16 +43,43 @@ var itemIds = [];
 
             $.ajax(baseURL + endpoint).done(function(topItems) {
                 itemIds = topItems;
+                offset = 0;
+                itemList = [];
+                $('#content').empty();
                 getMoreItems(topItems, offset);
+                if (section === 'newest') {
+                    startUpdates();
+                } else {
+                    stopUpdates();
+                }
             });
             lastUpdateTime = Date.now();
             $('#update_notification').addClass('hidden');
+        }
 
-            if (section === 'newest') {
-                startUpdates();
-            } else {
-                stopUpdates();
-            }
+        function getMoreItems(ids, start) {
+            var end = Math.min(start + itemLimit, ids.length);
+            var itemsToLoad = ids.slice(start, end);
+
+            var requests = itemsToLoad.map(function(id) {
+                return $.ajax(baseURL + '/item/' + id + '.json');
+            });
+
+            $.when.apply($, requests).done(function() {
+                var results = Array.prototype.slice.call(arguments);
+                results.forEach(function(result) {
+                    var item = result[0];
+                    itemList.push(item);
+                    $('#content').append(entryFormat(item));
+                });
+
+                offset = end;
+                busy = false;
+
+                if (end >= ids.length) {
+                    $('#scroll_text').text('No more items to load');
+                }
+            });
         }
 
         function formatDateTime(unixTimestamp) {
@@ -63,26 +90,35 @@ var itemIds = [];
         function updateNewestStories() {
             if (currentSection === 'newest') {
                 $.ajax(baseURL + '/newstories.json').done(function(newItems) {
-                    var newStories = newItems.slice(0, 20);
-                    var requests = newStories.map(function(item) {
-                        return $.ajax(baseURL + '/item/' + item + '.json');
-                    });
-
-                    $.when.apply($, requests).done(function() {
-                        var results = Array.prototype.slice.call(arguments, 0).map(function(array) {
-                            return array[0];
+                    var latestItemId = newItems[0];
+                    if (latestItemId > itemIds[0]) {
+                        var newStories = newItems.filter(id => id > itemIds[0]).slice(0, 20);
+                        var requests = newStories.map(function(item) {
+                            return $.ajax(baseURL + '/item/' + item + '.json');
                         });
 
-                        $('#content').empty();
-                        results.forEach(function(result) {
-                            $('#content').append(entryFormat(result));
+                        $.when.apply($, requests).done(function() {
+                            var results = Array.prototype.slice.call(arguments, 0).map(function(array) {
+                                return array[0];
+                            });
+
+                            // Sort results by time, latest first
+                            results.sort(function(a, b) {
+                                return b.time - a.time;
+                            });
+
+                            results.forEach(function(result) {
+                                $('#content').prepend(entryFormat(result));
+                                itemIds.unshift(result.id);
+                                itemList.unshift(result);
+                            });
+
+                            $('#update_notification').text(results.length + ' new stories added!').removeClass('hidden');
+                            setTimeout(function() {
+                                $('#update_notification').addClass('hidden');
+                            }, 3000);
                         });
-                        $('#content').removeClass('hidden');
-                        $('#update_notification').text('Newest stories updated!').removeClass('hidden');
-                        setTimeout(function() {
-                            $('#update_notification').addClass('hidden');
-                        }, 3000);
-                    });
+                    }
                 });
             }
         }
@@ -94,204 +130,205 @@ var itemIds = [];
                 if (currentSection === 'newest') {
                     updateNewestStories();
                 }
-            }, 5000);
+            }, 30000); // Check every 30 seconds
         }
 
         function stopUpdates() {
             clearInterval(updateInterval);
         }
 
-        /**
-         * Grabs more items from the top 100 list, with the given offset.
-         */
-        function getMoreItems(topItems, offset) {
-           topItems = topItems.slice(offset, offset + itemLimit);
-
-           var requests = topItems.map(function(item) {
-              var itemData = $.ajax(baseURL + '/item/' + item + '.json');
-
-              return itemData;
-           });
-
-           $.when.apply($, requests).done(function() {
-              var results = Array.prototype.slice.call(arguments, 0).map(function(array) {
-                 return array[0];
-              });
-
-              results.forEach(function(result) {
-                 itemList.push(result);
-                 $('#content').append(entryFormat(result));
-              });
-           });
-
-           this.offset += 20;
-           if (this.offset >= 100) {
-              $('#scroll_text').hide();
-              $(window).unbind('scroll');
-           }
-           busy = false;
-        }
-
-        /**
-         * Throws all parts of an item div into an array and then joins it with spaces.
-         */
         function entryFormat(data, full) {
-           var link = data.url ? 
-               '<a class="lead" target="_blank" href="' + data.url + '">' + data.title + '</a>' :
-               '<a class="lead" href="#" onclick="viewItem(this)" data-id="' + data.id + '">' + data.title + '</a>',
-               comments = data.kids ? data.kids.length : 0,
-               commentLink =  full ? ''
+            var link;
+            if (data.type === 'poll') {
+                link = '<a class="lead" href="#" onclick="viewItem(this)" data-id="' + data.id + '">' + data.title + '</a>';
+            } else {
+                link = data.url ? 
+                '<a class="lead" target="_blank" href="' + data.url + '">' + data.title + '</a>' :
+                '<a class="lead" href="#" onclick="viewItem(this)" data-id="' + data.id + '">' + data.title + '</a>';
+            }
+            var comments = data.kids ? data.kids.length : 0;
+            var commentLink =  full ? ''
                 : '| <b><span class="comment_link" data-id="' + data.id + '" onclick="viewItem(this)">' + comments + ' comments</span></b>',
-               dateTime = '<span class="post-date">' + formatDateTime(data.time) + '</span>',
-               entryArgs = [
-                 '<div class="item_entry">',
-                 link,
-                 '<p>',
-                 data.score,
-                 'points',
-                 ' by ' + data.by,
-                 ' | ' + dateTime,
-                 commentLink,
-                 '</p>',
-                 '</div>',
-              ];
+                dateTime = '<span class="post-date">' + formatDateTime(data.time) + '</span>',
+                entryArgs = [
+                '<div class="item_entry">',
+                link,
+                '<p>',
+                data.score,
+                'points',
+                ' by ' + data.by,
+                ' | ' + dateTime,
+                commentLink,
+                '</p>',
+                '</div>',
+            ];
 
-           var blurb = entryArgs.join(' ');
-           var extra = '<p>' + (data.text || '') + '</p>' + '<h3>' + comments + ' comments</h3>';
+            var blurb = entryArgs.join(' ');
+            var extra = '<p>' + (data.text || '') + '</p>' + '<h3>' + comments + ' comments</h3>';
 
-           if (full)
-              return blurb + extra;
-           return blurb;
+            if (full)
+                return blurb + extra;
+            return blurb;
         };
 
-        // ITEM SPECIFIC STUFF
+        function getPollOptions(pollId) {
+            return $.ajax(baseURL + '/item/' + pollId + '.json').then(function(poll) {
+                var optionPromises = poll.parts.map(function(partId) {
+                    return $.ajax(baseURL + '/item/' + partId + '.json');
+                });
+                
+                return $.when.apply($, optionPromises).then(function() {
+                    var options = Array.prototype.slice.call(arguments);
+                    var optionsHtml = '<div class="poll-options">';
+                    options.forEach(function(option) {
+                        optionsHtml += '<div class="poll-option">';
+                        optionsHtml += '<p>' + option[0].text + '</p>';
+                        optionsHtml += '<p>Score: ' + option[0].score + '</p>';
+                        optionsHtml += '</div>';
+                    });
+                    optionsHtml += '</div>';
+                    return optionsHtml;
+                });
+            });
+        }
 
-        // Grab item from id, populate item_fields with information
         function viewItem(item) {
-           var id = $(item).data('id'),
-               item = itemList.filter(function(item) {return item.id === id;} ).pop(),
-               numComments = item.kids ? item.kids.length : 0;
+            var id = $(item).data('id'),
+                item = itemList.filter(function(item) {return item.id === id;} ).pop(),
+                numComments = item.kids ? item.kids.length : 0;
 
-           history.pushState({}, "", "comments/" + id);
-           $('#item_meta').html(entryFormat(item, /* full */ true));
-           $('#front_page').addClass('hidden');
-           $('#title').addClass('hidden');
-           $('#navbar').addClass('hidden');
-           $('#item').removeClass('hidden');
-           $('#back_button').removeClass('hidden');
-           pageType = 'post';
+            history.pushState({}, "", "item/" + id);
+            $('#item_meta').html(entryFormat(item, /* full */ true));
+            $('#front_page').addClass('hidden');
+            $('#title').addClass('hidden');
+            $('#navbar').addClass('hidden');
+            $('#item').removeClass('hidden');
+            $('#back_button').removeClass('hidden');
+            pageType = 'post';
 
-           // Use $.when.apply($, objs) to wait for multiple objects
-           getTopComments(item);
+            if (item.type === 'poll') {
+                getPollOptions(id).then(function(pollOptionsHtml) {
+                    $('#item_meta').append(pollOptionsHtml);
+                });
+            }
+
+            getTopComments(item);
         }
 
         function getTopComments(item) {
-           var commentIds = item.kids;
+            var commentIds = item.kids;
 
-           $('#comment_field').empty();
+            $('#comment_field').empty();
 
-           // Return if no comments
-           if (!commentIds) {
-              return;
-           }
+            // Return if no comments
+            if (!commentIds) {
+                return;
+            }
 
-           getCommentsRecursive(commentIds, $('#comment_field'));
+            getCommentsRecursive(commentIds, $('#comment_field'));
         }
 
         function getCommentsRecursive(commentIds, parentElement) {
-           if (!commentIds || commentIds.length === 0) {
-              return $.Deferred().resolve();
-           }
+            if (!commentIds || commentIds.length === 0) {
+                return $.Deferred().resolve();
+            }
 
-           var requests = commentIds.map(function(commentId) {
-              return $.ajax(baseURL + '/item/' + commentId + '.json');
-           });
+            var requests = commentIds.map(function(commentId) {
+                return $.ajax(baseURL + '/item/' + commentId + '.json');
+            });
 
-           return $.when.apply($, requests).then(function() {
-              var comments = Array.prototype.slice.call(arguments);
-              var results = comments.map(function(comment) {
-                 return comment[0];
-              });
+            return $.when.apply($, requests).then(function() {
+                var comments = Array.prototype.slice.call(arguments);
+                var results = comments.map(function(comment) {
+                    return comment[0];
+                }).filter(function(comment) {
+                    return comment !== undefined;
+                });
 
-              var promises = results.map(function(comment) {
-                 var commentElement = createCommentElement(comment);
-                 parentElement.append(commentElement);
+                // Sort comments by time, latest first
+                results.sort(function(a, b) {
+                    return b.time - a.time;
+                });
 
-                 if (comment.kids && comment.kids.length > 0) {
-                    var childrenContainer = $('<div class="comment_children"></div>');
-                    commentElement.append(childrenContainer);
-                    return getCommentsRecursive(comment.kids, childrenContainer);
-                 } else {
-                    return $.Deferred().resolve();
-                 }
-              });
+                var promises = results.map(function(comment) {
+                    var commentElement = createCommentElement(comment);
+                    parentElement.append(commentElement);
 
-              return $.when.apply($, promises);
-           });
+                    if (comment && comment.kids && comment.kids.length > 0) {
+                        var childrenContainer = $('<div class="comment_children"></div>');
+                        commentElement.append(childrenContainer);
+                        return getCommentsRecursive(comment.kids, childrenContainer);
+                    } else {
+                        return $.Deferred().resolve();
+                    }
+                });
+
+                return $.when.apply($, promises);
+            });
         }
 
         function createCommentElement(comment) {
-           var text = comment.deleted ? '[Deleted]' : comment.text;
-           var by = '<strong>' + (comment.deleted ? '[Deleted]' : comment.by) + '</strong>';
-           var time = '<span class="time_since">' + getDateSincePost(comment.time) + '</span>';
-           return $('<div class="comment_blurb"><p>' + by + ' ' + time + '</p><p>' + text + '</p></div>');
+            if (!comment.text || comment.deleted) {
+                return $('<div class="comment_blurb"><p><strong>[Deleted]</strong></p><p>[This comment has been deleted]</p></div>');
+            }
+            var text = comment.text ;
+            var by = '<strong>' + comment.by + '</strong>';
+            var time = '<span class="time_since">' + getDateSincePost(comment.time) + '</span>';
+            var commentElement = $('<div class="comment_blurb"></div>');
+            commentElement.append($('<p>' + by + ' ' + time + '</p>'));
+            
+            commentElement.append($('<p>' + text + '</p>'));
+            return commentElement;
         }
 
         function backToFrontPage() {
-           history.back();
-           $('#item').addClass('hidden');
-           $('#back_button').addClass('hidden');
-           $('#front_page').removeClass('hidden');
-           $('#title').removeClass('hidden');
-           $('#navbar').removeClass('hidden');
-           pageType = 'list';
+            history.back();
+            $('#item').addClass('hidden');
+            $('#back_button').addClass('hidden');
+            $('#front_page').removeClass('hidden');
+            $('#title').removeClass('hidden');
+            $('#navbar').removeClass('hidden');
+            pageType = 'list';
         }
 
-        /**
-         * Display time difference in days, hours, or minutes.
-         */
         function getDateSincePost(postDate) {
-           var timeSince = (Date.now() / 1000) - postDate;
-           var days = Math.floor(timeSince / (60 * 60 * 24));
+            var timeSince = (Date.now() / 1000) - postDate;
+            var days = Math.floor(timeSince / (60 * 60 * 24));
 
-           if (days)
-              return days + " days ago";
+            if (days)
+                return days + " days ago";
 
-           var hours = Math.floor(timeSince / (60 * 60));
+            var hours = Math.floor(timeSince / (60 * 60));
 
-           if (hours)
-              return hours + " hours ago";
+            if (hours)
+                return hours + " hours ago";
 
-           var minutes = Math.floor(timeSince / 60);
+            var minutes = Math.floor(timeSince / 60);
 
-           return minutes + " minutes ago";
+            return minutes + " minutes ago";
         }
 
         $(window).scroll(function() {
-           clearTimeout(scrollThrottle);
-           scrollThrottle = setTimeout(function() {
-              if (pageType === 'list') {
-                 if (!busy && $(window).scrollTop() + $(window).height() == $(document).height()) {
-                    busy = true;
-                    getMoreItems(itemIds, offset);
-                 }
-                 if ($(window).scrollTop() === 0) {
-                    $('#update_notification').addClass('hidden');
-                 }
-              }
-           }, 300);
+            clearTimeout(scrollThrottle);
+            scrollThrottle = setTimeout(function() {
+                if (pageType === 'list') {
+                    if (!busy && $(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+                        busy = true;
+                        getMoreItems(itemIds, offset);
+                    }
+                    if ($(window).scrollTop() === 0) {
+                        $('#update_notification').addClass('hidden');
+                    }
+                }
+            }, 300);
         });
 
-        /**
-         * Wait until it has been 300 ms since the last AJAX completion to show the
-         * content.
-         */
         $(document).ajaxComplete(function() {
-           clearTimeout(ajaxTimeout);
-           ajaxTimeout = setTimeout(function() {
-              $('#content').removeClass('hidden');
-              $('#scroll_text').removeClass('hidden');
-           }, 300);
+            clearTimeout(ajaxTimeout);
+            ajaxTimeout = setTimeout(function() {
+                $('#content').removeClass('hidden');
+                $('#scroll_text').removeClass('hidden');
+            }, 300);
         });
 
         // Initial load
